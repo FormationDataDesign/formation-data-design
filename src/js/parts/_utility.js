4820f8f2af17231a872800105829e2fa256228c6
Eric Wiseman <eric@formationdatadesign.com>
2021-08-23 16:11:42 -0600

    lots of updates

--- src/js/parts/_utility.js
+++ src/js/parts/_utility.js
@@ -1,14 +1,16 @@
-import { throttle } from 'throttle-debounce';
-import rafl from 'rafl';
+import { throttle } from "throttle-debounce";
+import rafl from "rafl";
 
 const $ = window.jQuery;
 const tween = window.TweenMax;
-const isMobile = /Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/g.test( navigator.userAgent || navigator.vendor || window.opera );
+const isMobile = /Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/g.test(
+    navigator.userAgent || navigator.vendor || window.opera
+);
 
-const $wnd = $( window );
-const $doc = $( document );
-const $body = $( 'body' );
-const $html = $( 'html' );
+const $wnd = $(window);
+const $doc = $(document);
+const $body = $("body");
+const $html = $("html");
 let wndW;
 let wndH;
 let docH;
@@ -18,29 +20,32 @@ function getWndSize() {
     docH = $doc.height();
 }
 getWndSize();
-$wnd.on( 'resize load orientationchange', getWndSize );
+$wnd.on("resize load orientationchange", getWndSize);
 
 /**
  * Debounce resize
  */
 const resizeArr = [];
 function debounceResized() {
-    if ( resizeArr.length ) {
-        for ( let k = 0; k < resizeArr.length; k += 1 ) {
+    if (resizeArr.length) {
+        for (let k = 0; k < resizeArr.length; k += 1) {
             resizeArr[k]();
         }
     }
 }
-$wnd.on( 'ready load resize orientationchange', throttle( 200, () => {
-    rafl( debounceResized );
-} ) );
+$wnd.on(
+    "ready load resize orientationchange",
+    throttle(200, () => {
+        rafl(debounceResized);
+    })
+);
 debounceResized();
 
-function debounceResize( func ) {
-    if ( 'function' === typeof func ) {
-        resizeArr.push( func );
+function debounceResize(func) {
+    if ("function" === typeof func) {
+        resizeArr.push(func);
     } else {
-        window.dispatchEvent( new Event( 'resize' ) );
+        window.dispatchEvent(new Event("resize"));
     }
 }
 
@@ -53,39 +58,45 @@ let lastST = 0;
 function hasScrolled() {
     const ST = $wnd.scrollTop();
 
-    let type = ''; // [up, down, end, start]
+    let type = ""; // [up, down, end, start]
 
-    if ( ST > lastST ) {
-        type = 'down';
-    } else if ( ST < lastST ) {
-        type = 'up';
+    if (ST > lastST) {
+        type = "down";
+    } else if (ST < lastST) {
+        type = "up";
     } else {
-        type = 'none';
+        type = "none";
     }
 
-    if ( 0 === ST ) {
-        type = 'start';
-    } else if ( ST >= document.documentElement.offsetHeight - window.innerHeight ) {
-        type = 'end';
+    if (0 === ST) {
+        type = "start";
+    } else if (
+        ST >=
+        document.documentElement.offsetHeight - window.innerHeight
+    ) {
+        type = "end";
     }
 
-    hideOnScrollList.forEach( ( value ) => {
-        if ( 'function' === typeof value ) {
-            value( type, ST, lastST, $wnd );
+    hideOnScrollList.forEach((value) => {
+        if ("function" === typeof value) {
+            value(type, ST, lastST, $wnd);
         }
-    } );
+    });
 
     lastST = ST;
 }
 
-$wnd.on( 'scroll ready load resize orientationchange', throttle( 200, () => {
-    if ( hideOnScrollList.length ) {
-        rafl( hasScrolled );
-    }
-} ) );
+$wnd.on(
+    "scroll ready load resize orientationchange",
+    throttle(200, () => {
+        if (hideOnScrollList.length) {
+            rafl(hasScrolled);
+        }
+    })
+);
 
-function throttleScroll( callback ) {
-    hideOnScrollList.push( callback );
+function throttleScroll(callback) {
+    hideOnScrollList.push(callback);
 }
 
 /**
@@ -105,84 +116,113 @@ let originalBodyStyle;
 
 function bodyGetScrollbarWidth() {
     // thx d.walsh
-    const scrollDiv = document.createElement( 'div' );
-    scrollDiv.className = 'body-scrollbar-measure';
-    $body[0].appendChild( scrollDiv );
+    const scrollDiv = document.createElement("div");
+    scrollDiv.className = "body-scrollbar-measure";
+    $body[0].appendChild(scrollDiv);
     const result = scrollDiv.offsetWidth - scrollDiv.clientWidth;
-    $body[0].removeChild( scrollDiv );
+    $body[0].removeChild(scrollDiv);
     return result;
 }
 function bodyCheckScrollbar() {
     let fullWindowWidth = window.innerWidth;
-    if ( !fullWindowWidth ) {
-        const documentElementRect = document.documentElement.getBoundingClientRect();
-        fullWindowWidth = documentElementRect.right - Math.abs( documentElementRect.left );
+    if (!fullWindowWidth) {
+        const documentElementRect =
+            document.documentElement.getBoundingClientRect();
+        fullWindowWidth =
+            documentElementRect.right - Math.abs(documentElementRect.left);
     }
     isBodyOverflowing = $body[0].clientWidth < fullWindowWidth;
     scrollbarWidth = bodyGetScrollbarWidth();
 }
 
 function bodySetScrollbar() {
-    if ( 'undefined' === typeof originalBodyStyle ) {
-        originalBodyStyle = $body.attr( 'style' ) || '';
+    if ("undefined" === typeof originalBodyStyle) {
+        originalBodyStyle = $body.attr("style") || "";
     }
 
-    const originalBodyPadding = parseFloat( parseInt( document.documentElement.style.getPropertyValue( '--tmb-scrollbar-width' ), 10 ) ) || 0;
+    const originalBodyPadding =
+        parseFloat(
+            parseInt(
+                document.documentElement.style.getPropertyValue(
+                    "--tmb-scrollbar-width"
+                ),
+                10
+            )
+        ) || 0;
 
-    if ( 0 < scrollbarWidth ) {
-        document.documentElement.style.setProperty( '--tmb-scrollbar-width', `${scrollbarWidth + originalBodyPadding}px` );
+    if (0 < scrollbarWidth) {
+        document.documentElement.style.setProperty(
+            "--tmb-scrollbar-width",
+            `${scrollbarWidth + originalBodyPadding}px`
+        );
     }
 }
 function bodyResetScrollbar() {
-    $body.attr( 'style', originalBodyStyle );
-    document.documentElement.style.setProperty( '--tmb-scrollbar-width', '0px' );
+    $body.attr("style", originalBodyStyle);
+    document.documentElement.style.setProperty("--tmb-scrollbar-width", "0px");
 }
-function bodyOverflow( enable ) {
-    if ( enable && !bodyOverflowEnabled ) {
+function bodyOverflow(enable) {
+    if (enable && !bodyOverflowEnabled) {
         bodyOverflowEnabled = 1;
         bodyCheckScrollbar();
         bodySetScrollbar();
-        $body.css( 'overflow', 'hidden' );
-    } else if ( !enable && bodyOverflowEnabled ) {
+        $body.css("overflow", "hidden");
+    } else if (!enable && bodyOverflowEnabled) {
         bodyOverflowEnabled = 0;
-        $body.css( 'overflow', '' );
+        $body.css("overflow", "");
         bodyResetScrollbar();
     }
 }
-
 
 /**
  * In Viewport checker
  * return visible percent from 0 to 1
  */
-function isInViewport( $item, returnRect ) {
+function isInViewport($item, returnRect) {
     const rect = $item[0].getBoundingClientRect();
     let result = 1;
 
-    if ( 0 >= rect.right || rect.left >= wndW ) {
+    if (0 >= rect.right || rect.left >= wndW) {
         result = 0;
-    } else if ( 0 > rect.bottom && rect.top <= wndH ) {
+    } else if (0 > rect.bottom && rect.top <= wndH) {
         result = 0;
     } else {
-        const beforeTopEnd = Math.max( 0, rect.height + rect.top );
-        const beforeBottomEnd = Math.max( 0, rect.height - ( rect.top + rect.height - wndH ) );
-        const afterTop = Math.max( 0, -rect.top );
-        const beforeBottom = Math.max( 0, rect.top + rect.height - wndH );
-        if ( rect.height < wndH ) {
-            result = 1 - ( afterTop || beforeBottom ) / rect.height;
-        } else if ( beforeTopEnd <= wndH ) {
+        const beforeTopEnd = Math.max(0, rect.height + rect.top);
+        const beforeBottomEnd = Math.max(
+            0,
+            rect.height - (rect.top + rect.height - wndH)
+        );
+        const afterTop = Math.max(0, -rect.top);
+        const beforeBottom = Math.max(0, rect.top + rect.height - wndH);
+        if (rect.height < wndH) {
+            result = 1 - (afterTop || beforeBottom) / rect.height;
+        } else if (beforeTopEnd <= wndH) {
             result = beforeTopEnd / wndH;
-        } else if ( beforeBottomEnd <= wndH ) {
+        } else if (beforeBottomEnd <= wndH) {
             result = beforeBottomEnd / wndH;
         }
         result = 0 > result ? 0 : result;
     }
-    if ( returnRect ) {
+    if (returnRect) {
         return [result, rect];
     }
     return result;
 }
 
 export {
-    $, tween, isMobile, $wnd, $doc, $html, $body, wndW, wndH, docH, debounceResize, throttleScroll, bodyOverflow, bodyGetScrollbarWidth, isInViewport,
+    $,
+    tween,
+    isMobile,
+    $wnd,
+    $doc,
+    $html,
+    $body,
+    wndW,
+    wndH,
+    docH,
+    debounceResize,
+    throttleScroll,
+    bodyOverflow,
+    bodyGetScrollbarWidth,
+    isInViewport,
 };
--- src/js/themebau.js
+++ src/js/themebau.js
@@ -1,15 +1,15 @@
-import { initShowScroll } from './parts/initShowScroll';
-import { initCursor } from './parts/initCursor';
-import { initNavbar } from './parts/initNavbar';
-import { initInteractiveLinks } from './parts/initInteractiveLinks';
+import { initShowScroll } from "./parts/initShowScroll";
+import { initCursor } from "./parts/initCursor";
+import { initNavbar } from "./parts/initNavbar";
+import { initInteractiveLinks } from "./parts/initInteractiveLinks";
 
-import { initPluginSwiper } from './parts/initPluginSwiper';
-import { initPluginAnime } from './parts/initPluginAnime';
-import { initPluginImagesLoaded } from './parts/initPluginImagesLoaded';
-import { initPluginRellax } from './parts/initPluginRellax';
-import { initPluginIsotope } from './parts/initPluginIsotope';
-import { initPluginFancybox } from './parts/initPluginFancybox';
-import { initPluginOFI } from './parts/initPluginOFI';
+import { initPluginSwiper } from "./parts/initPluginSwiper";
+import { initPluginAnime } from "./parts/initPluginAnime";
+import { initPluginImagesLoaded } from "./parts/initPluginImagesLoaded";
+import { initPluginRellax } from "./parts/initPluginRellax";
+import { initPluginIsotope } from "./parts/initPluginIsotope";
+import { initPluginFancybox } from "./parts/initPluginFancybox";
+import { initPluginOFI } from "./parts/initPluginOFI";
 
 class THEMEBAU {
     init() {
@@ -30,50 +30,48 @@ class THEMEBAU {
         return self;
     }
 
-
     initShowScroll() {
-        return initShowScroll.call( this );
+        return initShowScroll.call(this);
     }
 
     initCursor() {
-        return initCursor.call( this );
+        return initCursor.call(this);
     }
 
     initNavbar() {
-        return initNavbar.call( this );
+        return initNavbar.call(this);
     }
 
     initInteractiveLinks() {
-        return initInteractiveLinks.call( this );
+        return initInteractiveLinks.call(this);
     }
 
-
     initPluginSwiper() {
-        return initPluginSwiper.call( this );
+        return initPluginSwiper.call(this);
     }
 
     initPluginAnime() {
-        return initPluginAnime.call( this );
+        return initPluginAnime.call(this);
     }
 
     initPluginImagesLoaded() {
-        return initPluginImagesLoaded.call( this );
+        return initPluginImagesLoaded.call(this);
     }
 
     initPluginRellax() {
-        return initPluginRellax.call( this );
+        return initPluginRellax.call(this);
     }
 
     initPluginIsotope() {
-        return initPluginIsotope.call( this );
+        return initPluginIsotope.call(this);
     }
 
     initPluginFancybox() {
-        return initPluginFancybox.call( this );
+        return initPluginFancybox.call(this);
     }
 
     initPluginOFI() {
-        return initPluginOFI.call( this );
+        return initPluginOFI.call(this);
     }
 }
 
